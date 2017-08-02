da7ca5c9f094179a7d5a80fb5a653ea514e55061
Jahid Ahmed <ahmedjahid@gmail.com>
2017-08-02 11:02:58 -0400

    add test .net application

---
+++ test/WebApplication2/WebApplication2/Samples/GoC.WebTemplate.C3.Sample/NestedMasterPageSample.aspx.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using GoC.WebTemplate;
+
+    public partial class NestedMasterPageSample1 : GoC.WebTemplate.BasePage
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            //set the page title
+            this.WebTemplateMaster.WebTemplateCore.HeaderTitle = "Nested Master Page Sample";
+        }
+    }
